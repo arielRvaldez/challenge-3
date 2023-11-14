ddf21ae3aab468412e5c4438757d8b6ddb7632f5
arielRvaldez <arielroxanne@outlook.com>
2023-11-13 18:07:29 -0600

    added javascript code

--- script.js
+++ script.js
@@ -1,19 +1,61 @@
 // Red Button
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // functions
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts = getPrompts();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+    if (correctPrompts) {
+      var password = generatePassword(); 
+      passwordText.value = password;
+    } 
+    else {
+      passwordText.value = "";
+    }
+    return true; 
 }
+
+var characterLength = 8;
+var choiceArr = [];
+
+var specialCharArr = ['!','@','#','$','%','^','&','*','(',')','+','=','~','{','}','[',']'];
+var lowerCassArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','S','T','U','V','W','X','Y','Z'];
+var numberArr = ['0','1','2','3','4','5','6','7','8','9'];
+
+function getPrompts(){
+  choiceArr = [];
+  characterLength = parseInt(prompt("Choose a password length between 8 to 128 characters"));
+
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+  alert("Please enter a number between 8 to 128. Please try again.");}
+   
+  if (confirm("Would you like lowercase letters included in your password?")) {
+    choiceArr =choiceArr.concat(lowerCassArr);
+  }
+  if (confirm("Would you like uppercase letters included in your password?")) {
+    choiceArr =choiceArr.concat(upperCaseArr);
+  }
+
+  if (confirm("Would you like special characters included in your password?")) {
+    choiceArr =choiceArr.concat(specialCharArr);
+  }
+
+  if (confirm("Would you like numbers included in your password?")) {
+    choiceArr =choiceArr.concat(numberArr);
+  }
+  return true;
+
 function generatePassword() {
-  console.log();
-
-  return "";
+  var password = "";
+  password += upperCaseArr[Math.floor(Math.random() * upperCaseArr.characterLength)]
+   for (var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length)
+    password = password + choiceArr[randomIndex];
+  }
+  return password;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+}
